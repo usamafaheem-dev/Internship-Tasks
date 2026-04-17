8512e452b7bf0c3333e61aa41fcbfea7156ebb60
Usama Faheem <usamafaheem9797@gmail.com>
2026-04-17 23:49:57 +0500

    update task-4

--- Day_4_Task_1/src/App.jsx
+++ Day_4_Task_1/src/App.jsx
@@ -5,13 +5,17 @@ import InputUse from "./Task7/InputUse";
 import RefCount from "./FinalProject/RefCount";
 import PreviousValueExample from "./FinalProject/PrevValue";
 import Timer from "./FinalProject/Timer";
+import LuxuryStopwatch from "./StopWatch/StopWatch";
+import VideoPlayerParent from "./Task3/VideoPlayerParent";
 
 const App = () => {
   return (
-    <div className="flex gap-4 justify-between items-center">
+    <div className="">
       {/* <PreviousValueExample /> */}
-      <Timer />
+      {/* <Timer /> */}
       {/* <RefCount /> */}
+      <LuxuryStopwatch/>
+      <VideoPlayerParent/>
     </div>
   );
 };
